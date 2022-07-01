6186d93516f511d02c2b8e4ac0e6549c79b8f201
robotdognz <robotdognz@gmail.com>
2022-07-02 09:14:49 +1200

    Forgot to use wall prefab, fixed

--- Assets/Scripts/RoomTemplates.cs
+++ Assets/Scripts/RoomTemplates.cs
@@ -10,4 +10,6 @@ public class RoomTemplates : MonoBehaviour
     public GameObject[] leftRooms;
 
     public int roomCount = 10;
+
+    // rooms should have references to their neighbors, graph structure. This will have lots of uses I can think of when I'm not absolutely wrecked.
 }
